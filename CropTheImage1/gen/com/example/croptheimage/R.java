bde7916fe26f1fbb798fbc7d4d56b6bdbd7c3270
LongNice <scoser6510@gmail.com>
2014-07-20 11:16:06 +0800

    大圖亂數+擴增為4*4imageview listener

--- CropTheImage1/gen/com/example/croptheimage/R.java
+++ CropTheImage1/gen/com/example/croptheimage/R.java
@@ -12,7 +12,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int fourteen=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int hundredseventynine=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int sixtynine=0x7f020003;
+        public static final int threeseventyeight=0x7f020004;
     }
     public static final class id {
         public static final int LinearLayout2=0x7f060000;
--- CropTheImage1/src/com/example/croptheimage/MainActivity.java
+++ CropTheImage1/src/com/example/croptheimage/MainActivity.java
@@ -21,10 +21,10 @@ import android.widget.*;
 import android.view.View;
 
 public class MainActivity extends Activity {
-	private ImageView imageView1;
+	/*private ImageView imageView1;
 	private ImageView imageView2;
 	private ImageView imageView3;
-	private ImageView imageView4;
+	private ImageView imageView4*/;
 	private ImageView no1, no2;
 	private Bitmap chooseimage1;
 	private Bitmap chooseimage2;
@@ -39,19 +39,57 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		imageView1 = (ImageView) this.findViewById(R.id.imageView1);
+		int[] image_group = {R.drawable.fourteen,R.drawable.hundredseventynine,R.drawable.sixtynine,R.drawable.threeseventyeight};
+		int random = (int) (Math.random() * image_group.length);
+		ImageView imageView1 = (ImageView) this.findViewById(R.id.imageView1);
 		imageView1.setOnClickListener(new ImageViewListener());
-		imageView2 = (ImageView) this.findViewById(R.id.imageView2);
+		ImageView imageView2 = (ImageView) this.findViewById(R.id.imageView2);
 		imageView2.setOnClickListener(new ImageViewListener());
-		imageView3 = (ImageView) this.findViewById(R.id.imageView3);
+		ImageView imageView3 = (ImageView) this.findViewById(R.id.imageView3);
 		imageView3.setOnClickListener(new ImageViewListener());
-		imageView4 = (ImageView) this.findViewById(R.id.imageView4);
+		ImageView imageView4 = (ImageView) this.findViewById(R.id.imageView4);
 		imageView4.setOnClickListener(new ImageViewListener());
+		ImageView imageView5 = (ImageView) this.findViewById(R.id.imageView5);
+		imageView5.setOnClickListener(new ImageViewListener());
+		ImageView imageView6 = (ImageView) this.findViewById(R.id.imageView6);
+		imageView6.setOnClickListener(new ImageViewListener());
+		ImageView imageView7 = (ImageView) this.findViewById(R.id.imageView7);
+		imageView7.setOnClickListener(new ImageViewListener());
+		ImageView imageView8 = (ImageView) this.findViewById(R.id.imageView8);
+		imageView8.setOnClickListener(new ImageViewListener());
+		ImageView imageView9 = (ImageView) this.findViewById(R.id.imageView9);
+		imageView9.setOnClickListener(new ImageViewListener());
+		ImageView imageView10 = (ImageView) this.findViewById(R.id.imageView10);
+		imageView10.setOnClickListener(new ImageViewListener());
+		ImageView imageView11 = (ImageView) this.findViewById(R.id.imageView11);
+		imageView11.setOnClickListener(new ImageViewListener());
+		ImageView imageView12 = (ImageView) this.findViewById(R.id.imageView12);
+		imageView12.setOnClickListener(new ImageViewListener());
+		ImageView imageView13 = (ImageView) this.findViewById(R.id.imageView13);
+		imageView13.setOnClickListener(new ImageViewListener());
+		ImageView imageView14 = (ImageView) this.findViewById(R.id.imageView14);
+		imageView14.setOnClickListener(new ImageViewListener());
+		ImageView imageView15 = (ImageView) this.findViewById(R.id.imageView15);
+		imageView15.setOnClickListener(new ImageViewListener());
+		ImageView imageView16 = (ImageView) this.findViewById(R.id.imageView16);
+		imageView16.setOnClickListener(new ImageViewListener());
 		ArrayList<Integer> print_random = new ArrayList<Integer>();
 		print_random.add(R.id.imageView1);
 		print_random.add(R.id.imageView2);
 		print_random.add(R.id.imageView3);
-		print_random.add(R.id.imageView4);//所有imageview的ID組
+		print_random.add(R.id.imageView4);
+		print_random.add(R.id.imageView5);
+		print_random.add(R.id.imageView6);
+		print_random.add(R.id.imageView7);
+		print_random.add(R.id.imageView8);
+		print_random.add(R.id.imageView9);
+		print_random.add(R.id.imageView10);
+		print_random.add(R.id.imageView11);
+		print_random.add(R.id.imageView12);
+		print_random.add(R.id.imageView13);
+		print_random.add(R.id.imageView14);
+		print_random.add(R.id.imageView15);
+		print_random.add(R.id.imageView16);//所有imageview的ID組
 		//取得目前時間
 		startTime = System.currentTimeMillis();
 		//設定定時要執行的方法
@@ -60,24 +98,26 @@ public class MainActivity extends Activity {
 		handler.postDelayed(updateTimer, 1000);//時間單位是毫秒
 		int sw = 0;// 切圖起使位址(X軸)
 		int sh = 0;// 切圖起使位址(Y軸)
-		for (int cut_no = 0; cut_no <= 4; cut_no++) {
-			Bitmap origialBitmap = BitmapFactory.decodeResource(getResources(),
-					R.drawable.fourteen);
-			int w = origialBitmap.getWidth() / 2;
-			int h = origialBitmap.getHeight() / 2;
-			if (sw == w*2) {
+		Bitmap origialBitmap = BitmapFactory.decodeResource(getResources(),
+				image_group[random]);
+		int w = origialBitmap.getWidth() / 4;
+		int h = origialBitmap.getHeight() / 4;
+		for (int cut_no = 0; cut_no <= 15; cut_no++) {
+			
+			if (sw == origialBitmap.getWidth()) {
 				sw = 0;
 				sh = sh + h;
-
+				
 			}
-			else{
+			
 			Bitmap cutBitmap = Bitmap.createBitmap(origialBitmap, sw, sh, w, h);// 前兩是原圖起始座標,後兩個是需求截圖的大小
-			int random = (int) (Math.random() * print_random.size());		
+			Log.i(TAG,"--"+sw+"--"+w);
+			random = (int) (Math.random() * print_random.size());		
 			ImageView random_cutimage = (ImageView) findViewById(print_random.get(random));// 隨機取任何一個imageview來呈現第一次切割的圖
 			random_cutimage.setImageBitmap(cutBitmap);
 			print_random.remove(random);
 			sw = sw + w;
-			}
+			
 		}
 
 	}
